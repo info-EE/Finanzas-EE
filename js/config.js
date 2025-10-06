400ad0adcc147a679eb29e97dab40544ba3712cf
Edgardo <info@europaenvios.com>
2025-10-06 16:13:02 -0300

    Ajustes 1

--- js/config.js
+++ js/config.js
@@ -2,6 +2,8 @@
 export const ESSENTIAL_INCOME_CATEGORIES = ['Ventas', 'Servicios', 'Otros Ingresos', 'Transferencia', 'Ajuste de Saldo'];
 export const ESSENTIAL_EXPENSE_CATEGORIES = ['Operaciones', 'Marketing', 'Salarios', 'Software', 'Impuestos', 'Otros Gastos', 'Inversión', 'Transferencia', 'Comisiones', 'Ajuste de Saldo'];
 export const ESSENTIAL_OPERATION_TYPES = ['Nacional / Intracomunitaria (UE)', 'Exportación (Fuera de la UE)'];
+export const ESSENTIAL_TAX_ID_TYPES = ['NIF', 'CIF', 'RUC', 'CUIT', 'EIN'];
 
 // --- Gráficos ---
 export const CHART_COLORS = ['#FFC3A0', '#FF7A85', '#D4A5A5', '#A5D4D4', '#CDB4DB', '#FFC8DD', '#A2D2FF', '#BDE0FE'];
+
